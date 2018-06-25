3688a60f0aa20140756864a43ec60bf1e87c21d3
Oroboros <ishaanpatkar@gmail.com>
2018-06-25 15:44:54 -0700

    Android Studio Project
    This is the basic Android Studio project.

---
+++ app/src/main/java/dignityonwheels/org/locator/Location.java
@@ -0,0 +1,17 @@
+package dignityonwheels.org.locator;
+
+public class Location {
+    private int day;
+    private String location;
+    public Location() {
+
+    }
+
+    public getDay() {
+        return day;
+    }
+
+    public getLocation() {
+        return location;
+    }
+}
